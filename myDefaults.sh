3c19e0dfedee3849f4f609d482abba3a95a6aaa9
secmeant <holsiarz@gmail.com>
2018-03-04 08:37:22 +0100

    Fixing shell script that isstalls vim things

--- myDefaults.sh
+++ myDefaults.sh
@@ -1,8 +1,8 @@
 apt-get install vim-gtk
 apt-get install git
+apt-get install g++
 mkdir my_defaults
 git clone https://github.com/secmeant/sets ./my_defaults/sets
-apt-get install g++
-cp -rf gvim $HOME/.vim
+cp -rf my_defaults/sets/gvim $HOME/.vim
 cp $HOME/.vim/vimrc $HOME/.vimrc
 mkdir $HOME/vimtmp
