1d4be469236ca1fac8b1167d4d416d602181d7c4
secmeant <patryk.wlazlyn@gmail.com>
2018-08-01 19:56:23 +0200

    making vimtmp directory hidden

--- myDefaults.sh
+++ myDefaults.sh
@@ -5,4 +5,4 @@ mkdir my_defaults
 git clone https://github.com/secmeant/sets ./my_defaults/sets
 cp -rf my_defaults/sets/gvim $HOME/.vim
 cp $HOME/.vim/vimrc $HOME/.vimrc
-mkdir $HOME/vimtmp
+mkdir $HOME/.vimtmp
