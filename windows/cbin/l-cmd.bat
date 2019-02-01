69b7e3d65920bebe4337096902666a285bc0bd25
secmeant <holsiarz@gmail.com>
2019-02-01 02:06:43 +0100

    l-cmd now uses kitty, conemu is super slow

--- windows/cbin/l-cmd.bat
+++ windows/cbin/l-cmd.bat
@@ -1,7 +1,7 @@
 @echo off
 
 IF "%1" == "" (
-	conemu "ssh holz@192.168.1.2"
+	start kitty -load vm holz@192.168.1.2
 ) ELSE (
- conemu "ssh %1@192.168.1.2"
+  start kitty -load vm %1@192.168.1.2
 )
