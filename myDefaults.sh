32301a588725b20e837789aa5a48f21f001a95d5
secmeant <secmeant@gmail.com>
2019-07-07 22:26:10 +0200

    Improving myDefaults script

--- myDefaults.sh
+++
@@ -1,8 +0,0 @@
-apt-get install vim-gtk
-apt-get install git
-apt-get install g++
-mkdir my_defaults
-git clone https://github.com/secmeant/sets ./my_defaults/sets
-cp -rf my_defaults/sets/gvim $HOME/.vim
-cp $HOME/.vim/vimrc $HOME/.vimrc
-mkdir $HOME/.vimtmp
---
+++ mydef.sh
@@ -0,0 +1,17 @@
+sudo apt-get install vim-gtk
+sudo apt-get install git
+sudo apt-get install g++
+
+if [[ "$(git remote show origin | head -n 2 | tail -n 1)" == *"https://github.com/secmeant/sets"* ]]; then
+	path_prefix="$(git rev-parse --show-toplevel)"
+else
+  mkdir my_defaults
+	git clone https://github.com/secmeant/sets ./my_defaults/sets
+	path_prefix="./my_defaults/sets"
+fi
+
+echo "path_prefix=$path_prefix"
+
+cp -rf "$path_prefix/gvim" $HOME/.vim
+cp $HOME/.vim/vimrc $HOME/.vimrc
+mkdir $HOME/.vimtmp
