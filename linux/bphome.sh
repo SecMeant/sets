4d3f4a9965aeba08a9270d8c32478c0fa9cec428
secmeant <secmeant@gmail.com>
2020-02-21 02:11:18 +0100

    Improving backup creation script
    If host is Arch distro, dumps installed packages to a file.
    Automatically excludes mountpoints from backups.

--- linux/bphome.sh
+++ linux/bphome.sh
@@ -10,17 +10,55 @@ if [ "$ans" != "$proper_ans" ]; then
 	exit 1;
 fi
 
+username=$(whoami)
 bpname="bph$(cat /etc/hostname)$(date +%d%m%Y)${$}.tar.gz"
 tmpbpname="/tmp/${bpname}"
+bpexclude_user="/home/${username}/.bpexclude"
+bpexclude="/tmp/bpexclude"
 color_default="\033[0m"
 color_done="\033[0;32m"
-username=$(whoami)
 taropt=""
 
-echo "Output file name: ${bpname}"
+if [ "$1" != "" ]; then
+	bptarget="$1"
+else
+	bptarget="/home/${username}"
+fi
+
+if [ "$2" != "" ]; then
+	tmpbpname="$2/${bpname}"
+	bpname="$2/${bpname}"
+fi
+
+distro=$(cat /etc/issue | cut -f 1 -d " ")
+
+if [ "${distro}" == "Arch" ]; then
+	echo "Current distro is Arch - dumping installed packages."
+	comm -23 <(pacman -Qeq | sort) <(pacman -Qgq base-devel | sort) > ~/.packages
+fi
+
+echo -n "Searching for mountpoits to exclude . . . "
+
+truncate ${bpexclude} --size 0
+cp ${bpexclude_user} ${bpexclude} 2>/dev/null
+bptarget_escaped=$(echo ${bptarget} | sed 's/\//\\\//g;s/ /\\ /g')
+cut -f 2 -d " " /proc/mounts | sed "s/$/\/\*/; /${bptarget_escaped}\/.*/ !d" >> ${bpexclude}
+
+echo -e "${color_done}DONE${color_default}"
+echo -e "Excluded dirs:"
+sed "s/^/\t/" "${bpexclude}"
+
+
+echo -e "\nOutput file name: ${bpname}"
 
 echo -n "Calculating size of data to compress . . . "
-total_size=$(du --exclude-from=/home/${username}/.bpexclude -sb /home/${username} | cut -f 1)
+
+if [ -f ${bpexclude} ]; then
+	total_size=$(du --exclude-from=${bpexclude} -sb ${bptarget} | cut -f 1)
+else
+	total_size=$(du -sb ${bptarget} | cut -f 1)
+fi
+
 checkpoint=$(echo ${total_size}/50000 | bc)
 echo -e "${color_done}DONE${color_default}"
 
@@ -35,8 +73,8 @@ echo "Copying and compressing. . . "
 
 # This probraby has to be the first appendted option
 # tar needs this at the very beggining
-if [ -f /home/${username}/.bpexclude ]; then
-	taropt="${taropt} --exclude-from=/home/${username}/.bpexclude"
+if [ -f ${bpexclude} ]; then
+	taropt="${taropt} --exclude-from=${bpexclude}"
 fi
 
 echo -n "Progress: ["
@@ -50,7 +88,7 @@ do
 	echo -ne "\x08"
 done
 
-tar $taropt -cp --record-size=1K --checkpoint="${checkpoint}" --checkpoint-action="ttyout=#" -f - /home/${username} 2>/dev/null | gzip > "${tmpbpname}"
+tar $taropt -cp --record-size=1K --checkpoint="${checkpoint}" --checkpoint-action="ttyout=#" -f - ${bptarget} 2>/dev/null | gzip > "${tmpbpname}"
 errc=$?
 if [ "${errc}" != "0" ]; then
 	echo "Error: tar exited with code ${errc}";
