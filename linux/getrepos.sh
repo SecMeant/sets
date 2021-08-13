5434bb651a5ad07ed2183ff7c199d8efb4cdc16f
secmeant <secmeant@gmail.com>
2021-08-13 10:40:01 +0200

    getrepos.sh: Now it just searches ~/etc/github for repos without any arguments

--- linux/getrepos.sh
+++ linux/getrepos.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
-if [ ! -f ~/.bprepos ]; then
-	exit 0
-fi
-
-while read repopath;
+for f in $(find ~/etc/github/ -maxdepth 1 -type d)
 do
-	for f in $(find /home/holz/etc/github/ -maxdepth 1 -type d)
-	do
-		pushd $f > /dev/null
-		url=$(git config --get remote.origin.url)
-		if [ ! -z "${url}" ]; then
-			echo -e "$f\t${url}"
-		fi
-		popd > /dev/null
-	done
-done < ~/.bprepos
+	pushd $f > /dev/null
+	url=$(git config --get remote.origin.url)
+	if [ ! -z "${url}" ]; then
+		echo -e "$f\t${url}"
+	fi
+	popd > /dev/null
+done
